5d8900e8f47dce34d049691e9d9d0a2ed653ed1a
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2026-02-08 15:25:52 +0530

    Time: 14 ms (33.58%), Space: 46.8 MB (90%) - LeetHub

--- 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.java
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.java
@@ -2,30 +2,29 @@ class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
         int i = 0;
         int j = 0;
-        int idx=0;
+        int idx = 0;
         int[] result = new int[nums.length - k + 1];
         HashMap<Integer, Integer> map = new HashMap<>();
         while (j < nums.length) {
             map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
             if (j - i + 1 == k) {
 
-                List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-
-                list.sort((a, b) -> {
+                PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> {
                     if (!a.getValue().equals(b.getValue())) {
                         return b.getValue() - a.getValue();
                     }
                     return b.getKey() - a.getKey();
                 });
 
-                int sum = 0;
-                int count = 0;
+                heap.addAll(map.entrySet());
 
-                for (Map.Entry<Integer, Integer> e : list) {
-                    if (count == x)
-                        break;
+                int sum = 0;
+                int cnt = 0;
+
+                while (!heap.isEmpty() && cnt < x) {
+                    Map.Entry<Integer, Integer> e = heap.poll();
                     sum += e.getKey() * e.getValue();
-                    count++;
+                    cnt++;
                 }
 
                 result[idx++] = sum;
