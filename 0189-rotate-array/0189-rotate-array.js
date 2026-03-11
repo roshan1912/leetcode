164d177e185707bb51ebbb5e36bf23f7b5d872e0
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2026-03-11 17:21:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    k = k % nums.length;
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+};
+
+const reverse = (nums, l, r) => {
+    while (l < r) {
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+        l++;
+        r--;
+    }
+}
