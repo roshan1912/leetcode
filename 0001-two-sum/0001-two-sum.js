9a7099ca9c79715930b035ba0c11ab7068617749
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2026-03-11 17:00:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const val = target - nums[i];
+
+        if (map.has(val)) {
+            return [map.get(val), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
