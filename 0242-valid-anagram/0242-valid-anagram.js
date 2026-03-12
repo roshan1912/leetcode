84b7618c8ceb564c5b6a07a9522395458dd9d38e
Roshan Yadav <91025355+roshan1912@users.noreply.github.com>
2026-03-13 00:20:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,17 +3,17 @@ var isAnagram = function (s, t) {
 
     const map = new Map();
 
-    for (let i = 0; i < s.length; i++) {
-        map.set(s[i], (map.get(s[i]) || 0) + 1);
+    for (const c of s) {
+        map.set(c, (map.get(c) || 0) + 1);
     }
 
-    for (let i = 0; i < t.length; i++) {
-        if (!map.has(t[i])) return false;
+    for (const c of t) {
+        if (!map.has(c)) return false;
 
-        map.set(t[i], map.get(t[i]) - 1);
+        map.set(c, map.get(c) - 1);
 
-        if (map.get(t[i]) === 0) {
-            map.delete(t[i]);
+        if (map.get(c) === 0) {
+            map.delete(c);
         }
     }
 
